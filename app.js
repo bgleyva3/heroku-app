c71c6b9ef30816fabc8bad60bf009b555fa35ae5
Brandon Leyva <90630618+BrandonLeyva@users.noreply.github.com>
2021-10-05 21:35:43 -0500

    third commit

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const app = express()
 
 //app.use importa el orden y entra a todos los middleware, con 
 //app.get, la url tiene que se exacta, por eso no importa el orden aquí
+
+app.get("/", (req, res) => {
+    console.log("heroku")
+    res.json({
+        message: "Welcome to my app"
+    })
+})
 app.get("/api/v1", (req, res) => {
     console.log("111")
     res.json({
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const app = require("./app")
 require("dotenv").config()
 
+//process.env.PORT es asignado nuevamente por heroku y nuestra app lo escucha ->
+// Si notros dejaramos 8000 como puerto fijo, no funcionaría en el deploy porque
+//heroku elige qué puedo abrir
 const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
