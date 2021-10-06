fb4b6321c6f69ade6faae0d135ec7c069e73f1f2
Brandon Leyva <90630618+BrandonLeyva@users.noreply.github.com>
2021-10-05 20:55:17 -0500

    second commit

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require("express")
 
 const app = express()
 
-app.use("/api/v1", (req, res) => {
+
+//app.use importa el orden y entra a todos los middleware, con 
+//app.get, la url tiene que se exacta, por eso no importa el orden aquí
+app.get("/api/v1", (req, res) => {
+    console.log("111")
     res.json({
         message: "Testing Heroku Deploy"
     })
 })
 
-app.use("/api/v1/users", (req, res) => {
+app.get("/api/v1/users", (req, res) => {
+    console.log("entró")
     res.json({
         id: 1,
         firstname: "Academlo",
--- server.js
+++ server.js
@@ -4,5 +4,5 @@ require("dotenv").config()
 const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-    console.log(`Servidor iniciado en el puerto ${PORT}``)
+    console.log(`Servidor iniciado en el puerto ${PORT}`)
 })
